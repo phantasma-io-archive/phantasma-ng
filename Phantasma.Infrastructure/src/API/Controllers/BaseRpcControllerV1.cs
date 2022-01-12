f40466e64579fb9ec1fbb22554e079fb8c7534b0
alexmpa <alexmpa@yandex.ru>
2022-01-12 21:12:44 +0300

    Add RPC support

---
+++ Phantasma.Infrastructure/src/API/Controllers/BaseRpcControllerV1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phantasma.Infrastructure.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("")]
+public abstract class BaseRpcControllerV1 : ControllerBase
+{
+}
---
+++ Phantasma.Infrastructure/src/API/Controllers/RpcController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Phantasma.Infrastructure.Controllers
+{
+    public class RpcController : BaseRpcControllerV1
+    {
+        public class RpcRequest
+        {
+            public string jsonrpc { get; set; }
+            public string method { get; set; }
+            public string id { get; set; }
+            public object[] @params { get; set; }
+        }
+
+        [APIInfo(typeof(object), "Returns query result.", false, 300)]
+        [HttpPost("rpc")]
+        public object Rpc(RpcRequest req)
+        {
+            object result = null;
+
+            try
+            {
+                // TODO implement something faster and more elegant
+
+                var controllers = Assembly.GetExecutingAssembly().GetTypes()
+                    .Where(type => typeof(BaseControllerV1).IsAssignableFrom(type));
+
+                foreach (var controller in controllers)
+                {
+                    var methodInfo = controller.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var method in methodInfo)
+                    {
+                        if(method.Name.ToLower() == req.method.ToLower())
+                        {
+                            var methodParameters = method.GetParameters();
+
+                            var processedParams = new List<object>();
+                            var i = 0;
+                            foreach (var param in req.@params)
+                            {
+                                if(param is JsonElement)
+                                {
+                                    if(methodParameters[i].ParameterType == typeof(string))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetString());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(Int32))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetInt32());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(UInt32))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetUInt32());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(Int64))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetInt64());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(UInt64))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetUInt64());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(Decimal))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetDecimal());
+                                    }
+                                    else if (methodParameters[i].ParameterType == typeof(bool))
+                                    {
+                                        processedParams.Add(((JsonElement)param).GetBoolean());
+                                    }
+                                    else
+                                    {
+                                        throw new Exception($"Unsupported parameter type {methodParameters[i].ParameterType}");
+                                    }
+                                }
+                                else
+                                {
+                                    processedParams.Add(param);
+                                }
+                                i++;
+                            }
+
+                            var instance = controller.GetConstructors().First().Invoke(null);
+                            result = method.Invoke(instance, processedParams.ToArray());
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (APIException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new APIException("RPC call exception", e);
+            }
+
+            return result;
+        }
+    }
+}
