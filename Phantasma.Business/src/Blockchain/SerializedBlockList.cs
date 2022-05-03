9ca6b5f4970cf70a170ae0515d6c60cd715dcefc
alexmpa <alexmpa@yandex.ru>
2022-05-03 19:55:43 +0300

    Small build fixes

--- Phantasma.Business/src/Blockchain/SerializedBlockList.cs
+++ Phantasma.Business/src/Blockchain/SerializedBlockList.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Numerics;
 using System.Collections.Generic;
 using Phantasma.Core;
+using System;
 
 namespace Phantasma.Business;
 
--- clean.sh
+++ clean.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm --force -r .vs
-rm --force -r Node/bin
-rm --force -r Node/obj
 rm --force -r Phantasma.Business/src/bin
 rm --force -r Phantasma.Business/src/obj
 rm --force -r Phantasma.Core/src/bin
@@ -13,9 +11,14 @@ rm --force -r Phantasma.Infrastructure/src/bin
 rm --force -r Phantasma.Infrastructure/src/obj
 rm --force -r Phantasma.Shared/src/bin
 rm --force -r Phantasma.Shared/src/obj
+rm --force -r Phantasma.Node/bin
+rm --force -r Phantasma.Node/obj
 rm --force -r Phantasma.Tests/bin
 rm --force -r Phantasma.Tests/obj
 rm --force -r Phantasma.Tests/Phantasma.Simulator/bin
 rm --force -r Phantasma.Tests/Phantasma.Simulator/obj
-rm --force -r Tendermint/src/bin
-rm --force -r Tendermint/src/obj
+rm --force -r Tendermint/bin
+rm --force -r Tendermint/obj
+rm --force -r Tendermint.RPC/bin
+rm --force -r Tendermint.RPC/obj
+
