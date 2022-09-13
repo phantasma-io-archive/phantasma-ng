a1ec11fe9b09aae95b861057bf99a7c5a24a8d23
Chris Eargle <kodefuguru@live.com>
2022-09-12 23:58:13 -0400

    Must abandon

---
+++ Phantasma.Simulator/Logging/LogKind.cs
@@ -0,0 +1,17 @@
+﻿namespace Phantasma.Core.Log
+{
+    /// <summary>
+    /// Represents the kind of the log message.
+    /// </summary>
+    public enum LogLevel
+    {
+        None = 0,
+        Success = 1,
+        Error = 2,
+        Warning = 3,
+        Message = 4,
+        Debug = 5,
+        Maximum = 6
+    }
+    // Don't change the order here, its important, must be ordered by importance
+}
