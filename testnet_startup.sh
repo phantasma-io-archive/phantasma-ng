b931d6f45953c03b92e396fa78668cc9f5b79d60
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-09-15 13:52:55 +0100

    Update testnet_startup.sh
    Fix script

--- testnet_startup.sh
+++ testnet_startup.sh
@@ -2,8 +2,8 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 # Stop old containers
-docker container stop phantasma-devnet
-docker container rm phantasma-devnet
+sudo docker container stop phantasma-devnet
+sudo docker container rm phantasma-devnet
 
 # Remove old images
 echo y | docker image prune -a
@@ -12,5 +12,8 @@ echo y | docker image prune -a
 chmod u+x ./build-docker.sh
 ./build-docker.sh
 
+sudo docker container stop phantasma-devnet
+sudo docker container rm phantasma-devnet
+
 # Run the testnet
-docker run --name phantasma-devnet -tid -p 5102:5102 -p 5101:5101 -p 26057:26057 phantasma-devnet
+sudo docker run --name phantasma-devnet -tid -p 5102:5102 -p 5101:5101 -p 26057:26057 phantasma-devnet
