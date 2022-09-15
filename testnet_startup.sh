a9ab01b419a6c53347cd9abea6a1800ff12afe95
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-09-15 14:30:30 +0100

    Auto deploy

--- testnet_startup.sh
+++ testnet_startup.sh
@@ -12,8 +12,5 @@ echo y | docker image prune -a
 chmod u+x ./build-docker.sh
 ./build-docker.sh
 
-sudo docker container stop phantasma-devnet
-sudo docker container rm phantasma-devnet
-
 # Run the testnet
 sudo docker run --name phantasma-devnet -tid -p 5102:5102 -p 5101:5101 -p 26057:26057 phantasma-devnet
