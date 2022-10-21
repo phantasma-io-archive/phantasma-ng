04bef98ae155725042c9dc4a89ed32bb656904fa
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-10-21 11:40:44 +0100

    Updating testnet startup
    Updating testnet startup, change the nexusname to testnet

--- build-docker-testnet-arm64.sh
+++ build-docker-testnet-arm64.sh
@@ -27,10 +27,10 @@ cp -R "$PUBLISH_ROOT" "$TESTNET_ROOT"/node1/publish
 cp -R "$PUBLISH_ROOT" "$TESTNET_ROOT"/node2/publish
 cp -R "$PUBLISH_ROOT" "$TESTNET_ROOT"/node3/publish
 
-cp -R "$TESTNET_ROOT"/node0/config_node0.json "$TESTNET_ROOT"/node0/publish/config.json
-cp -R "$TESTNET_ROOT"/node1/config_node1.json "$TESTNET_ROOT"/node1/publish/config.json
-cp -R "$TESTNET_ROOT"/node2/config_node2.json "$TESTNET_ROOT"/node2/publish/config.json
-cp -R "$TESTNET_ROOT"/node3/config_node3.json "$TESTNET_ROOT"/node3/publish/config.json
+cp -R "$TESTNET_ROOT"/node0/config_testnet_node0.json "$TESTNET_ROOT"/node0/publish/config.json
+cp -R "$TESTNET_ROOT"/node1/config_testnet_node1.json "$TESTNET_ROOT"/node1/publish/config.json
+cp -R "$TESTNET_ROOT"/node2/config_testnet_node2.json "$TESTNET_ROOT"/node2/publish/config.json
+cp -R "$TESTNET_ROOT"/node3/config_testnet_node3.json "$TESTNET_ROOT"/node3/publish/config.json
 
 docker build --platform=linux/arm64 -t phantasma-devnet -f DOCKER/DockerfileTestnetARM64 .
 
