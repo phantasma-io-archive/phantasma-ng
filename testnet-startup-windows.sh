1c8b1b847a153833b0de03039a3b46327e856f9e
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-10-25 14:17:44 +0100

    Update to Docker for windows

---
+++ testnet-startup-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Stop old containers
+docker container stop phantasma-devnet
+docker container rm phantasma-devnet
+
+# Remove old images
+echo y | docker image prune -a
+
+# Run the build script
+./build-docker-windows.sh
+
+# Run the testnet
+docker run --name phantasma-devnet -v $(pwd)/DOCKER/testnet:/app/testnet -tid -p 5102:5102 -p 5101:5101 -p 26057:26057 phantasma-devnet
