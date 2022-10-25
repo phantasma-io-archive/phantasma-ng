bd787d0f597d4ef134509dba7addcc5c7338077a
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-10-25 16:04:08 +0100

    Update Windows for dockers

--- build-docker-testnet-windows.bat
+++ build-docker-testnet-windows.bat
@@ -5,6 +5,7 @@ SET TESTNET_ROOT=%CD%\DOCKER\testnet
 SET PUBLISH_ROOT=%CD%\Phantasma.Node\bin\Debug\net6.0\linux-x64\publish\
 SET NODE_PROJ=Phantasma.Node\Phantasma.Node.csproj
 SET LAST_COMMIT=git rev-parse --short HEAD
+for /f %%x in ('git rev-parse --short HEAD') do set LAST_COMMIT=%%x
 curl "-LJO" "https:/\github.com\tendermint\tendermint\releases\download\v"%VERSION%"\tendermint_"%VERSION%"_linux_amd64.tar.gz" "-s" "-o" "tendermint_"%VERSION%"_linux_amd64.tar.gz"
 mkdir "-p" "DOCKER\bin"
 tar "-xzf" "tendermint_"%VERSION%"_linux_amd64.tar.gz" "-C" "%CD%\DOCKER\bin\"
@@ -22,5 +23,5 @@ COPY  "%TESTNET_ROOT%"\node0\config_testnet_node0.json "%TESTNET_ROOT%"\node0\pu
 COPY  "%TESTNET_ROOT%"\node1\config_testnet_node1.json "%TESTNET_ROOT%"\node1\publish\config.json
 COPY  "%TESTNET_ROOT%"\node2\config_testnet_node2.json "%TESTNET_ROOT%"\node2\publish\config.json
 COPY  "%TESTNET_ROOT%"\node3\config_testnet_node3.json "%TESTNET_ROOT%"\node3\publish\config.json
-docker "build" "--platform=linux\x86_64" "-t" "phantasma-devnet" "-f" "DOCKER\DockerfileTestnet" "%CD%"
-docker "tag" "phantasma-devnet:latest" "phantasmaio\phantasma-devnet:%LAST_COMMIT%"
+docker build --platform=linux\x86_64 -t "phantasma-devnet" -f "DOCKER\DockerfileTestnet" .
+docker tag phantasma-devnet:latest "phantasmaio\phantasma-devnet:%LAST_COMMIT%"
