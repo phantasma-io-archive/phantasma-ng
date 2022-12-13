e142392eb353bafee45b231a25391de35f22e0e1
João Novo <34069419+TeknoPT@users.noreply.github.com>
2022-12-13 07:24:43 +0000

    Added More tests and Imported LinkSimulator from the Old Chain to the updated one.

--- Phantasma.Core/tests/Utils/BitUtilsTests.cs
+++ Phantasma.Core/tests/Utils/BitUtilsTests.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Phantasma.Core.Tests.Utils;
 
 using Phantasma.Core.Utils;
@@ -35,11 +37,12 @@ public class BitUtilsTests
         Assert.Equal(buffer2, buffer);
     }
     
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertDoubleToLittleEndianByteArray()
     {
         // Arrange
         var value = 1234.5678;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x74, 0x33, 0x6B, 0x3F, 0xD0, 0x00, 0x00, 0x00 };
         var bytes = new byte[8];
 
@@ -47,14 +50,15 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
     
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertFloatToLittleEndianByteArray()
     {
         // Arrange
         var value = 123.456f;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x49, 0xC3, 0xF5, 0x40 };
         var bytes = new byte[4];
 
@@ -62,7 +66,7 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
 
     [Fact]
@@ -85,6 +89,7 @@ public class BitUtilsTests
     {
         // Arrange
         var value = (ushort)1234;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0xD2, 0x04 };
         var bytes = new byte[2];
 
@@ -92,14 +97,15 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
 
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertIntToLittleEndianByteArray()
     {
         // Arrange
         var value = 12345678;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x2E, 0xBC, 0x61, 0x00 };
         var bytes = new byte[4];
 
@@ -107,14 +113,15 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
 
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertUIntToLittleEndianByteArray()
     {
         // Arrange
         var value = (uint)12345678;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x2E, 0xBC, 0x61, 0x00 };
         var bytes = new byte[4];
 
@@ -122,14 +129,15 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
 
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertLongToLittleEndianByteArray()
     {
         // Arrange
         var value = 123456789012345;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x15, 0xCD, 0x5B, 0x07, 0xE1, 0x41, 0x00, 0x00 };
         var bytes = new byte[8];
 
@@ -137,14 +145,15 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
     
-    [Fact (Skip = "fix this later")]
+    [Fact]
     public void GetBytes_ShouldConvertULongToLittleEndianByteArray()
     {
         // Arrange
         var value = (ulong)123456789012345;
+        var expectedBytes = BitConverter.GetBytes(value);
         var expected = new byte[] { 0x15, 0xCD, 0x5B, 0x07, 0xE1, 0x41, 0x00, 0x00 };
         var bytes = new byte[8];
 
@@ -152,6 +161,6 @@ public class BitUtilsTests
         BitUtils.GetBytes(bytes, 0, value);
 
         // Assert
-        Assert.Equal(expected, bytes);
+        Assert.Equal(expectedBytes, bytes);
     }
 }
