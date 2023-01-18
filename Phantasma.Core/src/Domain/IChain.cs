b5683527d46bba99c9dfd5e6258308586900310f
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-01-18 15:37:09 +0000

    Update IChain.cs

--- Phantasma.Core/src/Domain/IChain.cs
+++ Phantasma.Core/src/Domain/IChain.cs
@@ -20,7 +20,7 @@ public interface IChain
     bool IsRoot { get; }
     IContract[] GetContracts(StorageContext storage);
     void AddBlock(Block block, IEnumerable<Transaction> transactions, StorageChangeSetContext changeSet);
-    void SetBlock(Block block, IEnumerable<Transaction> transactions);
+    byte[]  SetBlock(Block block, IEnumerable<Transaction> transactions);
 
     BigInteger GetTokenBalance(StorageContext storage, IToken token, Address address);
     BigInteger GetTokenBalance(StorageContext storage, string symbol, Address address);
