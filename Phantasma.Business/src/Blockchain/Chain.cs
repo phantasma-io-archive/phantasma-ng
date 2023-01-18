435e621d2ef5c516e7ad8698ffac33d723f07065
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-01-18 16:18:38 +0000

    Update Chain.cs
    Fix to the commit

--- Phantasma.Business/src/Blockchain/Chain.cs
+++ Phantasma.Business/src/Blockchain/Chain.cs
@@ -428,6 +428,8 @@ namespace Phantasma.Business.Blockchain
         public byte[] Commit()
         {
             Log.Information("Committing block {Height}", this.CurrentBlock.Height);
+            Block lastBlock = this.CurrentBlock;
+            
             try
             {
                 AddBlock(this.CurrentBlock, this.CurrentTransactions, this.CurrentChangeSet);
@@ -438,12 +440,6 @@ namespace Phantasma.Business.Blockchain
                 Log.Error("Critical failure {Error}", e);
                 Environment.Exit(-1);
             }
-
-            Block lastBlock = this.CurrentBlock;
-            this.CurrentBlock = null;
-            this.CurrentTransactions.Clear();
-
-            Log.Information("Committed block {Height}", lastBlock.Height);
 
             return lastBlock.Hash.ToByteArray();
         }
