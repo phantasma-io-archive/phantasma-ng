4971f7dce12e954f28fe1cad54b50f98a671b370
Sergio Flores <relfos@gmail.com>
2023-08-05 15:25:05 +0100

    TokenInfusion struct is now serializable

--- Phantasma.Core/src/Domain/Token/Structs/TokenInfusion.cs
+++ Phantasma.Core/src/Domain/Token/Structs/TokenInfusion.cs
@@ -1,15 +1,30 @@
+using Phantasma.Core.Domain.Interfaces;
+using Phantasma.Core.Utils;
+using System.IO;
 using System.Numerics;
 
 namespace Phantasma.Core.Domain.Token.Structs;
 
-public struct TokenInfusion
+public struct TokenInfusion: ISerializable
 {
-    public readonly string Symbol;
-    public readonly BigInteger Value;
+    public string Symbol;
+    public BigInteger Value;
 
     public TokenInfusion(string symbol, BigInteger value)
     {
         Symbol = symbol;
         Value = value;
+    }
+
+    public void SerializeData(BinaryWriter writer)
+    {
+        writer.WriteVarString(Symbol);
+        writer.WriteBigInteger(Value);
+    }
+
+    public void UnserializeData(BinaryReader reader)
+    {
+        Symbol = reader.ReadVarString();
+        Value = reader.ReadBigInteger();
     }
 }
